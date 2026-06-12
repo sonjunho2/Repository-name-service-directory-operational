eca22276a3dfcf3a4250bf91aa3225aba6524b8a
sonjunho2 <sonjunho2@gmail.com>
2026-06-12 14:12:02 +0900

    Update slider script for promo slides

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-let i=0; const slides=[...document.querySelectorAll('.slide')]; if(slides.length>1){setInterval(()=>{slides[i].classList.remove('on'); i=(i+1)%slides.length; slides[i].classList.add('on')},2800)}
+let i=0;
+const slides=[...document.querySelectorAll('.promo-slide')];
+if(slides.length>1){
+  setInterval(()=>{
+    slides[i].classList.remove('on');
+    i=(i+1)%slides.length;
+    slides[i].classList.add('on');
+  },2800);
+}
